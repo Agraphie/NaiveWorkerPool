a0e9049ef7323eb1f22fbd70abba79124ee7d718
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2018-11-03 16:33:01 +0100

    Uncomment test cases

--- workerPool_test.go
+++ workerPool_test.go
@@ -5,40 +5,39 @@ import (
 	"time"
 )
 
-//
-//func TestNewPool(t *testing.T) {
-//	initialWorkerCount := uint64(4)
-//	pool := Create(initialWorkerCount, 10)
-//
-//	if pool.workerCount != initialWorkerCount {
-//		t.Errorf("Worker sum was incorrect, got: %d, want: %d.", pool.workerCount, initialWorkerCount)
-//	}
-//}
-//
-//func TestDisposePool(t *testing.T) {
-//	initialWorkerCount := uint64(4)
-//	pool := Create(initialWorkerCount, 10)
-//	pool.Dispose()
-//
-//	if !pool.IsDisposed() {
-//		t.Errorf("Expected pool to be disposed, but was not.")
-//	}
-//}
-//
-//func TestDynamicallyGrowing(t *testing.T) {
-//	initialWorkerCount := uint64(4)
-//	maxWorker := uint64(10)
-//	pool := Create(initialWorkerCount, maxWorker)
-//	for i := 0; i < 100; i++ {
-//		pool.Submit(func() {
-//			time.Sleep(time.Second)
-//		})
-//	}
-//
-//	if pool.workerCount < maxWorker {
-//		t.Errorf("Worker sum was incorrect, got: %d, want: %d.", pool.workerCount, maxWorker)
-//	}
-//}
+func TestNewPool(t *testing.T) {
+	initialWorkerCount := uint64(4)
+	pool := Create(initialWorkerCount, 10)
+
+	if pool.workerCount != initialWorkerCount {
+		t.Errorf("Worker sum was incorrect, got: %d, want: %d.", pool.workerCount, initialWorkerCount)
+	}
+}
+
+func TestDisposePool(t *testing.T) {
+	initialWorkerCount := uint64(4)
+	pool := Create(initialWorkerCount, 10)
+	pool.Dispose()
+
+	if !pool.IsDisposed() {
+		t.Errorf("Expected pool to be disposed, but was not.")
+	}
+}
+
+func TestDynamicallyGrowing(t *testing.T) {
+	initialWorkerCount := uint64(4)
+	maxWorker := uint64(10)
+	pool := Create(initialWorkerCount, maxWorker)
+	for i := 0; i < 100; i++ {
+		pool.Submit(func() {
+			time.Sleep(time.Second)
+		})
+	}
+
+	if pool.workerCount < maxWorker {
+		t.Errorf("Worker sum was incorrect, got: %d, want: %d.", pool.workerCount, maxWorker)
+	}
+}
 
 func TestDynamicallyDecreasing(t *testing.T) {
 	initialWorkerCount := uint64(4)
